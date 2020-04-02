a91f13e4cfd634f725ea3a1971a58337f45ef13b
rigorio <rigorei.sarmiento@gmail.com>
2020-04-02 19:23:56 +0800

    Add project files.

---
+++ SalesSystem/Data/SalesSystemContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesSystem.Models;
+
+namespace SalesSystem.Data
+{
+    public class SalesSystemContext : DbContext
+    {
+        public SalesSystemContext (DbContextOptions<SalesSystemContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Sale> Sale { get; set; }
+        public DbSet<Gown> Gowns { get; set; }
+    }
+}
---
+++ SalesSystem/Models/Gown.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesSystem.Models
+{
+    public class Gown
+    {
+        public int Id { get; set; }
+        public String name { get; set; }
+        public String description { get; set; }
+        public Double price { get; set; }
+        public String status { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime dateRented { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime dueDate { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime dateReturned { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime pickupDate { get; set; }
+        public String contact { get; set; }
+        public String orNumber { get; set; }
+        public String address { get; set; }
+        public String partialPayment { get; set; }
+        public String balance { get; set; }
+        public String securityDeposit { get; set; }
+    }
+}
